5b2b63e455d7972451e6051c4906d338954f0ddf
Nandita Rajagopalan <nandita@Nanditas-MacBook-Pro.local>
2014-07-22 01:07:26 -0500

    reminder app: first pass.

---
+++ reminder/app/controllers/tasks_controller.rb
@@ -0,0 +1,29 @@
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all.order("created_at DESC")
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(set_params)
+    @task.save!
+  end
+
+  def complete
+    @task = Task.find(params[:id])
+    @task.done = true
+    @task.save
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+  end
+
+  private
+  def set_params
+    params.require(:task).permit(:title, :due_date)
+  end
+
+end
---
+++ reminder/app/models/task.rb
@@ -0,0 +1,4 @@
+class Task < ActiveRecord::Base
+  validates :title, presence:true
+  validates :done, inclusion: {in: [true, false]}
+end
